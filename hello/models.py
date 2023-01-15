1b7ad52d8bf7b23f037e4a14f2011795a70c3a6a
Owain Hughes <owain2.hughes@live.uwe.ac.uk>
2023-01-15 08:48:42 +0000

    Changes -oh

--- hello/models.py
+++ hello/models.py
@@ -9,3 +9,15 @@ class LogMessage(models.Model):
         date = timezone.localtime(self.log_date)
         return f"'{self.message}' logged on {date.strftime('%A, %d %B, %Y at %X')}"
     #migraate database
+
+class Movie(models.Model):
+    name = models.CharField(max_length=100)
+    rating = models.CharField(max_length=10)
+    def __str__(self):
+        return self.name
+
+class Screening(models.Model):
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
+    seats_available = models.IntegerField()
+    def __str__(self):
+        return self.seats_available
